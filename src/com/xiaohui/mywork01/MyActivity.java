c85ee23e655977d0cadf319f8a0fd5d574675cce
Xiaohui1201 <a6551142@163.com>
2015-09-18 20:11:50 +0800

    add an activity

---
+++ src/com/xiaohui/mywork01/MyActivity.java
@@ -0,0 +1,15 @@
+package com.xiaohui.mywork01;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by xiaohui on 15-9-18.
+ * email:a6551142@163.com
+ */
+public class MyActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
